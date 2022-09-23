41ed13d7562032fddca78c5161e800e590153cd3
Utsav Tayde <utsav@simplycompete.net>
2022-09-23 19:09:08 +0530

    convert a repo into front-end app

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-var cookieParser = require('cookie-parser')
+import fetch from 'node-fetch';
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-var corsOptions = {
-  origin: 'http://localhost:9000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-// app.use(cors(corsOptions));
-// app.use(cookieParser());
-
-app.get('/', (req, res) => {
-    // res.cookie('Authorization','utsav642',{
-    //     httpOnly: true,
-    //     sameSite: 'none'
-    // });    
-    res.send('Hello World, from express');
-});
-
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify({
+      sender: 'utsav642'
+    }),
+    headers: { 'Content-Type': 'application/json'}
+}).then(res => res.json())
+  .then(json => console.log(json));
