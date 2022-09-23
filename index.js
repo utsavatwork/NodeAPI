d919f1c5e99680eb1c17bba7a7f1c12c6a9066be
Utsav Tayde <utsav@simplycompete.net>
2022-09-23 19:58:19 +0530

    add fetch

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const express = require("express");
-const cors = require("cors");
-var cookieParser = require('cookie-parser')
+// const cors = require("cors");
+// var cookieParser = require('cookie-parser')
+import fetch from 'node-fetch';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-var corsOptions = {
-  origin: 'http://localhost:9000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// var corsOptions = {
+//   origin: 'http://localhost:9000',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
-// app.use(cors(corsOptions));
-// app.use(cookieParser());
+// // app.use(cors(corsOptions));
+// // app.use(cookieParser());
 
 app.get('/', (req, res) => {
     // res.cookie('Authorization','utsav642',{
     //     httpOnly: true,
     //     sameSite: 'none'
     // });    
+
+    fetch('http://localhost:8888/signup', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({
+          'email': 'utsav@gmaail.com',
+          'password': 'passsword'
+        }),
+        headers: { 'Content-Type': 'application/json'}
+    }).then(res => res.json())
+      .then(json => console.log(json));
+
     res.send('Hello World, from express');
 });
 
