1193131cecf2c6faca2ebaf7e2088eccc20f69c8
Utsav Tayde <utsav@simplycompete.net>
2022-09-23 16:15:59 +0530

    added cookie parser, and set cookie on / endpoint

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+var cookieParser = require('cookie-parser')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
+    res.cookie('Authorization','utsav642',{
+        httpOnly: true,
+        secure: true
+    });    
     res.send('Hello World, from express');
 });
 
